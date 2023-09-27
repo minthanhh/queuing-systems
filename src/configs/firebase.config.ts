d2ae46c77abc3f46df1e4e663d8b1312af27d56c
minthanhh <mint03sanzz@gmail.com>
2023-09-27 13:55:01 +0700

    Final

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,21 +1,20 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
-import { getStorage } from "firebase/storage";
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtOBTZ_Cmkhr6SeLUtjQzpd8zqN-UDc-E",
-  authDomain: "queuing-systems.firebaseapp.com",
-  projectId: "queuing-systems",
-  storageBucket: "queuing-systems.appspot.com",
-  messagingSenderId: "613623226327",
-  appId: "1:613623226327:web:30547ff8f41899b35fb592"
+   apiKey: 'AIzaSyCtOBTZ_Cmkhr6SeLUtjQzpd8zqN-UDc-E',
+   authDomain: 'queuing-systems.firebaseapp.com',
+   projectId: 'queuing-systems',
+   storageBucket: 'queuing-systems.appspot.com',
+   messagingSenderId: '613623226327',
+   appId: '1:613623226327:web:30547ff8f41899b35fb592',
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app)
-const auth = getAuth(app)
-const storage = getStorage(app)
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-
-export { db, auth, storage }
+export { db, auth, storage };
