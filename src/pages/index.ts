d96e149a3890c7e1f1730598bcc0424ea5c49b2b
minthanhh <mint03sanzz@gmail.com>
2023-09-27 14:00:00 +0700

    Final

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,44 +1,41 @@
-export { default as ForgotPassword } from './Auth/ForgotPassword'
-export { default as ResetPassword } from './Auth/ResetPassword'
+export { default as ForgotPassword } from './Auth/ForgotPassword';
+export { default as ResetPassword } from './Auth/ResetPassword';
 
-export { default as Login } from './Auth/Login' 
+export { default as Login } from './Auth/Login';
 
-
-
-// Root pages 
-export { default as Dashboard }from './root/Dashboard'
-export { default as Report }from './root/Report/Report'
-
+// Root pages
+export { default as Dashboard } from './root/Dashboard';
+export { default as Report } from './root/Report/Report';
 
 // Pages - devices
-export { default as Device }from './root/Devices/Device'
-export { default as AddDevice }from './root/Devices/AddDevice'
-export { default as DetailDevice }from './root/Devices/DetailDevice'
-export { default as UpdateDevice }from './root/Devices/UpdateDevice'
-export { default as DetailGiveNumber }from './root/Devices/DetailGiveNumber'
+export { default as Device } from './root/Devices/Device';
+export { default as AddDevice } from './root/Devices/AddDevice';
+export { default as DetailDevice } from './root/Devices/DetailDevice';
+export { default as UpdateDevice } from './root/Devices/UpdateDevice';
+export { default as DetailGiveNumber } from './root/Devices/DetailGiveNumber';
 
 // Pages - services
-export { default as Service }from './root/Services/Service'
-export {default as AddService} from './root/Services/AddService'
-export {default as DetailService} from './root/Services/DetailService'
-export {default as UpdateService} from './root/Services/UpdateService'
+export { default as Service } from './root/Services/Service';
+export { default as AddService } from './root/Services/AddService';
+export { default as DetailService } from './root/Services/DetailService';
+export { default as UpdateService } from './root/Services/UpdateService';
 
 // - Manager pages
 
 // Roles
-export { default as Role }from './root/Managers/Roles/Role'
-export { default as AddRole }from './root/Managers/Roles/AddRole'
-export { default as UpdateRole }from './root/Managers/Roles/UpdateRole'
+export { default as Role } from './root/Managers/Roles/Role';
+export { default as AddRole } from './root/Managers/Roles/AddRole';
+export { default as UpdateRole } from './root/Managers/Roles/UpdateRole';
 
 // Accounts
-export { default as Account }from './root/Managers/Accounts/Account'
-export { default as AddAccount }from './root/Managers/Accounts/AddAccount'
-export { default as UpdateAccount }from './root/Managers/Accounts/UpdateAccount'
+export { default as Account } from './root/Managers/Accounts/Account';
+export { default as AddAccount } from './root/Managers/Accounts/AddAccount';
+export { default as UpdateAccount } from './root/Managers/Accounts/UpdateAccount';
 
-// User Logs 
-export { default as UserLogs } from './root/UserLogs'
-export { default as NewNumber } from './root/Number/NewNumber'
+// User Logs
+export { default as UserLogs } from './root/UserLogs';
+export { default as NewNumber } from './root/Number/NewNumber';
 
 // Give Numbers
-export { default as GiveNumber } from './root/Number/GiveNumber'
-export { default as Profile } from './root/Profile'
+export { default as GiveNumber } from './root/Number/GiveNumber';
+export { default as Profile } from './root/Profile';
